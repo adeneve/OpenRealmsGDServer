1bf15aef947716916bd5c4dce7144cf09c725f52
Andrew de Neve <adeneve83@gmail.com>
2023-12-23 19:00:11 -0800

    basic server setup

---
+++ Server/Server.gd
@@ -0,0 +1,28 @@
+extends Node
+
+var PORT:int = 2000
+var MAX_CONNECTIONS:int = 30
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	multiplayer.peer_connected.connect(_on_player_connected)
+	multiplayer.peer_disconnected.connect(_on_player_disconnected)
+	var peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(PORT, MAX_CONNECTIONS)
+	if error:
+		return error
+	multiplayer.multiplayer_peer = peer
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+	
+	
+	
+func _on_player_connected(id):
+	print("player connected with id: %s" % id)
+	
+func _on_player_disconnected(id):
+	print("player disconnected with id: %s" % id)
+
