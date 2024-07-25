6e807698c403e28daa0720199da0cb70c6ce0a84
Andrew de Neve <adeneve83@gmail.com>
2024-07-24 21:52:37 -0700

    characters folder progress

--- Server/server.py
+++ Server/server.py
@@ -6,6 +6,7 @@ from os.path import isfile, join, exists
 app = Flask(__name__)
 
 app.config['WORLDS_FOLDER'] = 'Worlds'
+app.config['CHARACTERS_FOLDER'] = 'Characters'
 
 @app.route("/")
 def hello_world():
@@ -17,16 +18,24 @@ def download_file(name):
         app.config['WORLDS_FOLDER'], name
     )
 
+@app.route("/characters/<path:name>")
+def download_character_file(name):
+    return send_from_directory(
+        app.config['CHARACTERS_FOLDER'], name
+    )
+
 @app.route("/get_scene_texture_file_list/<username>/<world_name>")
 def get_scene_texture_file_list(username, world_name):
     path = "./"+ app.config['WORLDS_FOLDER'] + "/"+username+"/"+world_name+"/"+"mainScene/textures"
     if exists(path):
       return listdir(path)
 
-@app.route("/get_character_texture_file_list/<username>/<world_name>/<character_name>")
-def get_character_texture_file_list(username, world_name, character_name):
-    path = "./"+ app.config['WORLDS_FOLDER'] + "/"+username+"/"+world_name+"/characters/"+character_name+"/textures"
+@app.route("/get_character_texture_file_list/<character_name>")
+def get_character_texture_file_list(character_name):
+    path = "./"+ app.config['CHARACTERS_FOLDER'] + "/"+character_name+"/textures"
     if exists(path):
       return listdir(path)
     
 #TODO create a way for users to upload their own worlds
+
+# store animations as a json mapping? e.g. idle -> cat_idle, cat_idle invoked on client
